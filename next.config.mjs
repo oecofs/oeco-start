64e5a2684d407705398d83cdba86ab97ff10eed7
oecofs <oecofs@gmail.com>
2026-07-30 20:14:13 -0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Permite que o Next.js seja deployado na Vercel sem configuração extra
+  reactStrictMode: true,
+};
+
+export default nextConfig;
